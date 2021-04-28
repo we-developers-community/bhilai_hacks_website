834b45076c0b7b749dd11e7bbae30b9066170b26
Suvansh Rana <36293610+suvansh-rana@users.noreply.github.com>
2021-04-28 18:13:17 +0530

    Removed unused css and js

--- src/js/main.js
+++ src/js/main.js
@@ -136,47 +136,47 @@ const FlipDown = require("./flipdown");
     }
   });
 
-  const glightbox = GLightbox({
-    selector: ".glightbox",
-  });
+  // const glightbox = GLightbox({
+  //   selector: ".glightbox",
+  // });
 
-  new Swiper(".gallery-slider", {
-    speed: 400,
-    loop: true,
-    centeredSlides: true,
-    autoplay: {
-      delay: 5000,
-      disableOnInteraction: false,
-    },
-    slidesPerView: "auto",
-    pagination: {
-      el: ".swiper-pagination",
-      type: "bullets",
-      clickable: true,
-    },
-    breakpoints: {
-      320: {
-        slidesPerView: 1,
-        spaceBetween: 20,
-      },
-      575: {
-        slidesPerView: 2,
-        spaceBetween: 20,
-      },
-      768: {
-        slidesPerView: 3,
-        spaceBetween: 20,
-      },
-      992: {
-        slidesPerView: 5,
-        spaceBetween: 20,
-      },
-    },
-  });
+  // new Swiper(".gallery-slider", {
+  //   speed: 400,
+  //   loop: true,
+  //   centeredSlides: true,
+  //   autoplay: {
+  //     delay: 5000,
+  //     disableOnInteraction: false,
+  //   },
+  //   slidesPerView: "auto",
+  //   pagination: {
+  //     el: ".swiper-pagination",
+  //     type: "bullets",
+  //     clickable: true,
+  //   },
+  //   breakpoints: {
+  //     320: {
+  //       slidesPerView: 1,
+  //       spaceBetween: 20,
+  //     },
+  //     575: {
+  //       slidesPerView: 2,
+  //       spaceBetween: 20,
+  //     },
+  //     768: {
+  //       slidesPerView: 3,
+  //       spaceBetween: 20,
+  //     },
+  //     992: {
+  //       slidesPerView: 5,
+  //       spaceBetween: 20,
+  //     },
+  //   },
+  // });
 
-  const galleryLightbox = GLightbox({
-    selector: ".gallery-lightbox",
-  });
+  // const galleryLightbox = GLightbox({
+  //   selector: ".gallery-lightbox",
+  // });
 
   on("show.bs.modal", "#buy-ticket-modal", function (event) {
     select(
@@ -186,7 +186,7 @@ const FlipDown = require("./flipdown");
 
   window.addEventListener("load", () => {
     AOS.init({
-      duration: 1000,
+      duration: 700,
       easing: "ease-in-out",
       once: true,
       mirror: false,
